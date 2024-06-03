c042b84be1d5fa5378c4f67ba4702af40449a100
kordyak <kordyak@mail.ru>
2024-06-03 07:57:40 +0500

    Добавьте файлы проекта.

---
+++ Module1.vb
@@ -0,0 +1,35 @@
+﻿Imports System.IO
+Imports System.Text
+Imports System.Text.RegularExpressions
+
+Module Module1
+
+    Sub Main()
+
+        Console.WriteLine("EnterKey: ")
+        Dim key_new = Console.ReadLine
+
+
+        ' Читаем старый RDP
+        Dim Files() As String = IO.Directory.GetFiles(".\", "*.rdp", IO.SearchOption.TopDirectoryOnly)
+        Dim wsa_rdp = My.Computer.FileSystem.ReadAllText(Files(0))
+
+        ' Ищем старый ключ
+        Dim r = New Regex("gatewayaccesstoken:s:npr\\\w*-(\d*)")
+        Dim key_old As String = r.Match(wsa_rdp).Groups(1).Value
+        ' Заменяем его на новый
+        wsa_rdp = wsa_rdp.Replace(key_old, key_new)
+
+
+        ' Записываем файл
+        Dim fs As FileStream = File.Create(Files(0))
+        Dim info As Byte() = New UTF8Encoding(True).GetBytes(wsa_rdp)
+        fs.Write(info, 0, info.Length)
+        fs.Close()
+
+
+        Process.Start(fs.Name)
+
+    End Sub
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("Run_rdp")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Run_rdp")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("2bf155c0-08cf-42ca-8377-aaf7eaf6102a")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+' Можно задать все значения или принять номера сборки и редакции по умолчанию 
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
